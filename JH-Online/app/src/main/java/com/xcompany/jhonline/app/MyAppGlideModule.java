c7d91dfb6fb723cc04f76f047c51a97bdb9c092d
xieliang <1186654762@qq.com>
2018-11-25 22:43:40 +0800

    home

---
+++ JH-Online/app/src/main/java/com/xcompany/jhonline/app/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package com.xcompany.jhonline.app;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by xieliang on 2018/11/25 21:22
+ */
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+}
