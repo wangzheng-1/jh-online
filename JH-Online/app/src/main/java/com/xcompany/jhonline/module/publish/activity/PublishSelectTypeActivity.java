aa4457b02937cf945288a1a310270b241902f05e
wangzheng <wangzheng@ihappy.net.cn>
2018-12-30 17:11:41 +0800

    图片文件添加

--- JH-Online/app/src/main/java/com/xcompany/jhonline/module/publish/activity/PublishSelectTypeActivity.java
+++ JH-Online/app/src/main/java/com/xcompany/jhonline/module/publish/activity/PublishSelectTypeActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 import com.xcompany.jhonline.R;
@@ -25,43 +24,74 @@ public class PublishSelectTypeActivity extends BaseActivity {
     LinearLayout backHomeLayout;
     @BindView(R.id.titleLayout)
     RelativeLayout titleLayout;
-    @BindView(R.id.typeListView)
-    ListView typeListView;
 
     PublishTypeAdapter publishTypeAdapter;
+    @BindView(R.id.divideConstructionLayout)
+    LinearLayout divideConstructionLayout;
+    @BindView(R.id.deviceLeaseLayout)
+    LinearLayout deviceLeaseLayout;
+    @BindView(R.id.buildingMaterialsTradeLayout)
+    LinearLayout buildingMaterialsTradeLayout;
+    @BindView(R.id.certificateServiceLayout)
+    LinearLayout certificateServiceLayout;
+    @BindView(R.id.laborRecruitLayout)
+    LinearLayout laborRecruitLayout;
+    @BindView(R.id.skillRecruitLayout)
+    LinearLayout skillRecruitLayout;
+    @BindView(R.id.constructionMatchLayout)
+    LinearLayout constructionMatchLayout;
+    @BindView(R.id.majorSkillLibraryLayout)
+    LinearLayout majorSkillLibraryLayout;
+    @BindView(R.id.industryBlackListLayout)
+    LinearLayout industryBlackListLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_select_type);
         ButterKnife.bind(this);
-        initAdapter();
+//        initAdapter();
 
     }
-    private void initAdapter(){
-        List<Model> models = getData();
-        publishTypeAdapter = new PublishTypeAdapter(this,models);
-        typeListView.setAdapter(publishTypeAdapter);
-    }
+//    private void initAdapter(){
+//        List<Model> models = getData();
+//        publishTypeAdapter = new PublishTypeAdapter(this,models);
+//        typeListView.setAdapter(publishTypeAdapter);
+//    }
 
-    private List<Model> getData(){
+    private List<Model> getData() {
         List<Model> models = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             models.add(new Model());
         }
         return models;
     }
 
 
-    @OnClick({R.id.backHomeLayout, R.id.reportTitleText})
+    @OnClick({R.id.backHomeLayout,R.id.divideConstructionLayout, R.id.deviceLeaseLayout, R.id.buildingMaterialsTradeLayout, R.id.certificateServiceLayout, R.id.laborRecruitLayout, R.id.skillRecruitLayout, R.id.constructionMatchLayout, R.id.majorSkillLibraryLayout, R.id.industryBlackListLayout})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.backHomeLayout:
                 onBackPressed();
                 break;
+            case R.id.divideConstructionLayout:
+                break;
+            case R.id.deviceLeaseLayout:
+                break;
+            case R.id.buildingMaterialsTradeLayout:
+                break;
+            case R.id.certificateServiceLayout:
+                break;
+            case R.id.laborRecruitLayout:
+                break;
+            case R.id.skillRecruitLayout:
+                break;
+            case R.id.constructionMatchLayout:
+                break;
+            case R.id.majorSkillLibraryLayout:
+                break;
+            case R.id.industryBlackListLayout:
+                break;
         }
     }
-
-
-
 }
--- JH-Online/app/src/main/java/com/xcompany/jhonline/module/publish/activity/PublishSubpackageQualityTeamActivity.java
+++ JH-Online/app/src/main/java/com/xcompany/jhonline/module/publish/activity/PublishSubpackageQualityTeamActivity.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import com.xcompany.jhonline.R;
 import com.xcompany.jhonline.base.BaseActivity;
 
+/**
+ * 发布分包城建 匹配优质班组信息
+ */
 public class PublishSubpackageQualityTeamActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
